ec7d1f80f4b6c2b1e6f56503070effb336091835
ibrahimzkh <ibrahimmma@mail.ru>
2024-02-27 20:22:57 +0300

    first commit

--- index.js
+++ index.js
@@ -9,19 +9,47 @@ document.addEventListener('DOMContentLoaded', () => {
   const BREEDS_URL = "https://dog.ceo/api/breeds/list";
 
   // узел, в котором будет список изображений
-  const imagesContainer = document.querySelector('.images');
+  const imagesContainer = document.querySelector('.images')
+  imagesContainer.classList.add('images__item')
 
   // узел, в котором будет список пород
   const breedsContainer = document.querySelector('.breeds');
 
   // узел кнопки обновления
   const button = document.querySelector('button');
-
   // сразу загружаем изображения
+  function fetchAndRenderImages () {
+    imagesContainer.textContent = ''
+    fetch(IMAGES_URL)
+    .then(res => res.json())
+    .then((data) => {
+    for(let i = 0; i < data.message.length; i ++){
+      const image = document.createElement('img')
+      image.src = data.message[i]
+      imagesContainer.append(image)
+    }
+  })
+
+  };
   fetchAndRenderImages();
 
+
+
+
+
   // загружаем список пород
-  fetchBreedsList();
+  function fetchBreedsList(){
+    fetch(BREEDS_URL)
+    .then(res => res.json())
+    .then((data)=>{
+      for(let i = 0; i < data.message.length; i ++){
+        const breed = document.createElement('li')
+        breed.textContent = data.message[i]
+        breedsContainer.append(breed)
+      }
+  })
+  };
+  fetchBreedsList()
 
   // еще раз загружаем изображения, если кликнули на кнопку обновления
   button.addEventListener('click', () => {
